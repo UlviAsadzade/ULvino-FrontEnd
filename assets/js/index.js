b5a32b85d6a00e7cf20d9a3c126567c0c1e44402
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-11-17 14:21:49 +1300

    created testimonials section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,14 @@
-const leftArrow = document.querySelector(".bi-chevron-left")
-const rightArrow = document.querySelector(".bi-chevron-right")
+// Slider section start
+const leftArrow = document.querySelector(".slider .bi-chevron-left")
+const rightArrow = document.querySelector(".slider .bi-chevron-right")
 const slides = Array.from(document.querySelectorAll(".slider-item"))
 
-const clearActives = () => {
-    slides.forEach(slide => {
-        slide.classList.remove('active');
-    });
+// const clearActiveSlider = () => {
+//     slides.forEach(slide => {
+//         slide.classList.remove('active');
+//     });
 
-}
+// }
 
 rightArrow.onclick = function () {
     const currentActive = document.querySelector('main .slider-item.active');
@@ -33,3 +34,48 @@ leftArrow.onclick = function () {
         currentActive.previousElementSibling.classList.add('active');
     }
 }
+
+// slider section finish
+
+///////////////////////////////////////
+
+// Testimonials section start
+
+const left = document.querySelector(".testimonials .bi-chevron-left")
+const right = document.querySelector(".testimonials .bi-chevron-right")
+const feedbackBoxes = Array.from(document.querySelectorAll(".feedback-box"))
+
+const clearActives = () => {
+    feedbackBoxes.forEach(feedbackBox => {
+        feedbackBox.classList.remove('active');
+    });
+
+}
+
+right.onclick = function () {
+    const currentActive = document.querySelector('.testimonials .feedback-box.active');
+    if (currentActive == feedbackBoxes[feedbackBoxes.length-1]) {
+        currentActive.classList.remove('active');
+        feedbackBoxes[0].classList.add('active');
+
+    }else{
+        currentActive.classList.remove('active');
+        currentActive.nextElementSibling.classList.add('active');
+    }
+
+}
+
+left.onclick = function () {
+    const currentActive = document.querySelector('.testimonials .feedback-box.active');
+    if (currentActive == feedbackBoxes[0]) {
+        currentActive.classList.remove('active');
+        feedbackBoxes[feedbackBoxes.length-1].classList.add('active');
+
+    }else{
+        currentActive.classList.remove('active');
+        currentActive.previousElementSibling.classList.add('active');
+    }
+}
+
+
+// Testimonials section finish
