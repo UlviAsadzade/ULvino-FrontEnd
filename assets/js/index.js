040e794b82b21f6eb4f3e247013fabca9368e5c4
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-11-29 14:42:41 +1300

    created product-modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -101,4 +101,27 @@ targetButtons.forEach(targetButton => {
     }
 });
 
+
+const minus = document.querySelector(".minus")
+const plus = document.querySelector(".plus")
+
+minus.onclick = function(e){
+    e.preventDefault();
+    sum = minus.nextElementSibling.innerHTML;
+    if (sum == 1) {
+        minus.nextElementSibling.innerHTML = 1;
+    }
+    else {
+        sum--;
+        minus.nextElementSibling.innerHTML = sum;
+    }
+}
+
+plus.onclick = function(e){
+    e.preventDefault();
+    sum = plus.previousElementSibling.innerHTML;
+    sum++;
+    plus.previousElementSibling.innerHTML = sum;
+}
+
 // Feature products section finish
